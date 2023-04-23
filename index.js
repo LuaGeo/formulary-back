c692b56137a0c5ab87c361ca8a36341d3b14d2ac
Luana <ldeoliveiratech@gmail.com>
2023-04-23 14:01:24 +0200

    cors access added

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
 const express = require("express");
 const cors = require("cors");
 const formData = require("form-data");
-// const Mailgun = require("mailgun.js");
+const Mailgun = require("mailgun.js");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 ///////// MAILGUN CONFIG ////////////
 
-// const mailgun = new Mailgun(formData);
-// const client = mailgun.client({
-//   username: "qjsdkuqhfe",
-//   key: process.env.MAILGUN_API_KEY,
-// });
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({
+  username: "qjsdkuqhfe",
+  key: process.env.MAILGUN_API_KEY,
+});
 
-// app.get("/", async (req, res) => {
-//   res.send("server is up");
-// });
+app.get("/", (req, res) => {
+  res.send("server is up");
+});
 
-// app.post("/form", (req, res) => {
-//   console.log(req.body);
-//   try {
-//     const messageData = {
-//       from: `${req.body.firstname} ${req.body.lastname} <${req.body.email}>`,
-//       to: "ldeoliveiratech@gmail.com",
-//       subject: "Coucou !",
-//       text: req.body.message,
-//     };
+app.post("/form", async (req, res) => {
+  console.log(req.body);
+  try {
+    const messageData = {
+      from: `${req.body.firstname} ${req.body.lastname} <${req.body.email}>`,
+      to: "ldeoliveiratech@gmail.com",
+      subject: req.body.subject,
+      text: req.body.message,
+    };
 
-//     client.messages.create(process.env.MAILGUN_DOMAIN, messageData);
-//     console.log(response);
-//     res.json(response);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
+    const response = await client.messages.create(
+      process.env.MAILGUN_DOMAIN,
+      messageData
+    );
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.listen("3000", () => {
-  console.log("server started !! 🚀🚀👌");
+app.listen("3001", () => {
+  console.log("Server Started !! 🚀🚀👌");
 });
